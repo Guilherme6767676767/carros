8de5952732b14a1228f485378db127ad7e3745c1
Guilherme6767676767 <jaojapao8@gmail.com>
2026-03-09 17:05:46 -0300

    sgfd

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.162.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js';
+// Carregado via script global no index.html
 
 let scene, camera, renderer;
 let speed = 2.0;
